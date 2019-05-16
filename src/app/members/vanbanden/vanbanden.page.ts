8635d80b93cb9e2f504379bccc1cc0a62eb1d054
dunghoang046 <hoangthuc046@gmail.com>
2019-05-16 11:06:31 +0700

    dunghv

--- src/app/members/vanbanden/vanbanden.page.ts
+++ src/app/members/vanbanden/vanbanden.page.ts
@@ -7,27 +7,40 @@ import { Response } from 'selenium-webdriver/http';
   styleUrls: ['./vanbanden.page.scss'],
 })
 export class VanbandenPage implements OnInit {
-  public danhsachVanBans:any;
- public dataquery ={CurrentPage:1,RowPerPage:10};
+ danhsachVanBans:any;
+ total=0;
+ public dataquery ={CurrentPage:1,RowPerPage:10,SearchIn:'SoKyHieu,TrichYeu',Keyword:''};
   constructor(private authService:AuthenticationService) { }
 
   ngOnInit() {
-    var a =  this.authService.currentUser;
+   this.load();
+  }
+  load(){
     this.authService.getVanBanDen(this.dataquery).subscribe(res =>{
-      debugger;
-      this.danhsachVanBans = res["data"];
-
+      this.danhsachVanBans = res["Data"];
+      this.total=res["Total"]
+    });
+  }
+  loadsearch(){
+    this.dataquery.RowPerPage=10;
+    this.dataquery.CurrentPage=1;
+    this.authService.getVanBanDen(this.dataquery).subscribe(res =>{
+      this.danhsachVanBans = res["Data"];
+      this.total=res["Total"];
     });
   }
   doInfinite(infiniteScroll) {
     setTimeout(() => {
-      //console.log('Async operation has ended');
       if (this.dataquery.CurrentPage == 1)
-        this.dataquery.CurrentPage  = 2;
-        this.authService.getVanBanDen(this.dataquery).subscribe(res =>{
-          this.danhsachVanBans.push(res["data"]);
+        this.dataquery.CurrentPage  = 2;     
+        if((this.dataquery.CurrentPage * this.dataquery.RowPerPage)<this.total){
+        this.authService.getVanBanDen(this.dataquery).subscribe(res =>{          
+          for (var j = 0; j < res["Data"].length; j++) {
+            this.danhsachVanBans.push(res["Data"][j])
+          }
         });
         this.dataquery.CurrentPage  = this.dataquery.CurrentPage  + 1;
+      }
       infiniteScroll.target.complete();
     }, 500);
   }
