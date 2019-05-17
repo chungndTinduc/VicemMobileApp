9c887aabc2b465fdccddd2fc215b5284943dccdf
dunghoang046 <hoangthuc046@gmail.com>
2019-05-17 17:27:29 +0700

    dunghv

--- src/app/members/chitietvanbandi/chitietvanbandi.page.ts
+++ src/app/members/chitietvanbandi/chitietvanbandi.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-chitietvanbandi',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChitietvanbandiPage implements OnInit {
 
-  constructor() { }
+  vanban:{};
+  LtsAllDanhMuc:any;
+  public dataquery ={ID:0};
+  total=0;
+  ID:any;
+  constructor(private authService:AuthenticationService, private route: ActivatedRoute) { }
+
 
   ngOnInit() {
+  }
+  ionViewDidEnter(){
+    this.load();
+  }
+  load(){
+    this.dataquery.ID = parseInt(this.route.snapshot.paramMap.get('vanbandiid'));
+    this.authService.getVanBanDiByID(this.dataquery).subscribe(res =>{
+      this.vanban = res["Data"];
+      this.LtsAllDanhMuc=res["LtsAllDanhMuc"];
+    });
   }
 
 }
