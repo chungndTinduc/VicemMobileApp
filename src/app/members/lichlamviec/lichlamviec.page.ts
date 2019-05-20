7e3da29126e569b2c7098b3c8386b467f304e339
dunghoang046 <hoangthuc046@gmail.com>
2019-05-20 14:50:17 +0700

    dunghv

--- src/app/members/lichlamviec/lichlamviec.page.ts
+++ src/app/members/lichlamviec/lichlamviec.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-lichlamviec',
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lichlamviec.page.scss'],
 })
 export class LichlamviecPage implements OnInit {
-
-  constructor() { }
-
+  danhsachs:any;
+  total=0;
+  daterance='';
+  public dataquery ={week:'',year:''};
+   constructor(private authService:AuthenticationService) { }
+ 
   ngOnInit() {
+  
+  }
+  ionViewDidEnter(){
+    this.load();
+  }
+  load(){
+    this.authService.getLichLamViec(this.dataquery).subscribe(res =>{
+      this.danhsachs = res["Data"];
+      this.total=res["Total"];
+      this.dataquery.week=res["week"];
+      this.dataquery.year=res["year"];
+      this.daterance=res["daterance"];
+      return true;
+    });
+  }
+  prev(){
+      this.dataquery.week=(parseInt(this.dataquery.week)-1).toString();
+    this.authService.getLichLamViec(this.dataquery).subscribe(res =>{
+      this.danhsachs = res["Data"];
+      this.total=res["Total"];
+      this.dataquery.week=res["week"];
+      this.dataquery.year=res["year"];
+      this.daterance=res["daterance"];
+      return true;
+    });
+  }
+  next(){
+   this.dataquery.week=(parseInt(this.dataquery.week)+1).toString();
+  this.authService.getLichLamViec(this.dataquery).subscribe(res =>{
+    this.danhsachs = res["Data"];
+    this.total=res["Total"];
+    this.dataquery.week=res["week"];
+    this.dataquery.year=res["year"];
+    this.daterance=res["daterance"];
+    return true;
+  });
+}
+  loadsearch(){
+    this.authService.getLichLamViec(this.dataquery).subscribe(res =>{
+      this.danhsachs = res["Data"];
+      this.total=res["Total"];
+    });
   }
 
 }
