f14df9ebedef126ee35b554ebea70896f9a7371c
dunghoang046 <hoangthuc046@gmail.com>
2019-05-27 17:34:02 +0700

    dunghv

--- src/app/members/vanbanden/vanbanden.page.ts
+++ src/app/members/vanbanden/vanbanden.page.ts
@@ -27,6 +27,15 @@ export class VanbandenPage implements OnInit {
       return true;
     });
   }
+  dorefresh(refresher){
+    this.dataquery.RowPerPage=10;
+    this.dataquery.CurrentPage=1;
+    this.authService.getVanBanDen(this.dataquery).subscribe(res =>{
+      this.danhsachVanBans = res["Data"];
+      this.total=res["Total"];
+      refresher.target.complete();
+    });
+  }
   loadsearch(){
     this.dataquery.RowPerPage=10;
     this.dataquery.CurrentPage=1;
