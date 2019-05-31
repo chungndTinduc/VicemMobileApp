c0c083cbee8206c02680291e762026d9488b4f4a
chungndTinduc <tinduc.mobileApp@hotmail.com>
2019-05-31 09:22:18 +0700

    Update Cong viec page

---
+++ platforms/android/app/src/main/assets/www/congviec-danhsach-danhsach-module.js
@@ -0,0 +1,111 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["congviec-danhsach-danhsach-module"],{
+
+/***/ "./src/app/members/congviec/danhsach/danhsach.module.ts":
+/*!**************************************************************!*\
+  !*** ./src/app/members/congviec/danhsach/danhsach.module.ts ***!
+  \**************************************************************/
+/*! exports provided: DanhsachPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DanhsachPageModule", function() { return DanhsachPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _danhsach_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./danhsach.page */ "./src/app/members/congviec/danhsach/danhsach.page.ts");
+
+
+
+
+
+
+
+var routes = [
+    {
+        path: '',
+        component: _danhsach_page__WEBPACK_IMPORTED_MODULE_6__["DanhsachPage"]
+    }
+];
+var DanhsachPageModule = /** @class */ (function () {
+    function DanhsachPageModule() {
+    }
+    DanhsachPageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_5__["IonicModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_4__["RouterModule"].forChild(routes)
+            ],
+            declarations: [_danhsach_page__WEBPACK_IMPORTED_MODULE_6__["DanhsachPage"]]
+        })
+    ], DanhsachPageModule);
+    return DanhsachPageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/members/congviec/danhsach/danhsach.page.html":
+/*!**************************************************************!*\
+  !*** ./src/app/members/congviec/danhsach/danhsach.page.html ***!
+  \**************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header>\n  <ion-toolbar>\n    <ion-title>danhsach</ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n\n</ion-content>\n"
+
+/***/ }),
+
+/***/ "./src/app/members/congviec/danhsach/danhsach.page.scss":
+/*!**************************************************************!*\
+  !*** ./src/app/members/congviec/danhsach/danhsach.page.scss ***!
+  \**************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL21lbWJlcnMvY29uZ3ZpZWMvZGFuaHNhY2gvZGFuaHNhY2gucGFnZS5zY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/members/congviec/danhsach/danhsach.page.ts":
+/*!************************************************************!*\
+  !*** ./src/app/members/congviec/danhsach/danhsach.page.ts ***!
+  \************************************************************/
+/*! exports provided: DanhsachPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DanhsachPage", function() { return DanhsachPage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var DanhsachPage = /** @class */ (function () {
+    function DanhsachPage() {
+    }
+    DanhsachPage.prototype.ngOnInit = function () {
+    };
+    DanhsachPage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-danhsach',
+            template: __webpack_require__(/*! ./danhsach.page.html */ "./src/app/members/congviec/danhsach/danhsach.page.html"),
+            styles: [__webpack_require__(/*! ./danhsach.page.scss */ "./src/app/members/congviec/danhsach/danhsach.page.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], DanhsachPage);
+    return DanhsachPage;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=congviec-danhsach-danhsach-module.js.map
