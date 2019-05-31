bf3c9c59750ab636a586f19afac526d3fe11b907
chungndTinduc <tinduc.mobileApp@hotmail.com>
2019-05-31 14:48:02 +0700

    swipe tab

--- src/app/members/app-tabs/app-tabs.module.ts
+++ src/app/members/app-tabs/app-tabs.module.ts
@@ -1,104 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
-
 import { AppTabsPage } from './app-tabs.page';
-
-
-const routes: Routes = [
-  {
-    path: 'tabs',
-    component: AppTabsPage,
-    children: [
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full'
-      },
-      { path: 'dashboard', loadChildren: 'src/app/members/dashboard/dashboard.module#DashboardPageModule' },
-      { path: 'vanbanden',   
-      children: [
-        {
-          path: '',
-          loadChildren: 'src/app/members/vanbanden/vanbanden.module#VanbandenPageModule'
-        },
-        {
-          path: 'vanbandenchuaxuly',
-          loadChildren: 'src/app/members/vanbanden/vanbandenchuaxuly/vanbandenchuaxuly.module#VanbandenchuaxulyPageModule'
-        },
-        {
-          path: 'vanbandenchuadoc',
-          loadChildren: 'src/app/members/vanbanden/vanbandenchuadoc/vanbandenchuadoc.module#VanbandenchuadocPageModule'
-        },
-        {
-          path: 'vanbandenxulychinh',
-          loadChildren: 'src/app/members/vanbanden/vanbandenxulychinh/vanbandenxulychinh.module#VanbandenxulychinhPageModule'
-        },
-        {
-          path: 'chitietvanbanden/:vanbandenid',
-          loadChildren: 'src/app/members/vanbanden/chitietvanbanden/chitietvanbanden.module#ChitietvanbandenPageModule'
-        },
-      ]
-     },
-      { path: 'vanbandi',     
-       children: [
-        {
-          path: '',
-          loadChildren:'src/app/members/vanbandi/vanbandi.module#VanbandiPageModule',
-        },
-        {
-          path: 'vanbandichuadoc',
-          loadChildren:'src/app/members/vanbandichuadoc/vanbandichuadoc.module#VanbandichuadocPageModule',
-        },
-        {
-          path: 'chitietvanbandi/:vanbandiid',
-          loadChildren:'src/app/members/chitietvanbandi/chitietvanbandi.module#ChitietvanbandiPageModule',
-        },
-      ]
-       },
-      { path: 'congviec', loadChildren: 'src/app/members/congviec/congviec.module#CongviecPageModule' },
-      { path: 'lichlamviec', loadChildren: 'src/app/members/lichlamviec/lichlamviec.module#LichlamviecPageModule' },
-      {
-         path: 'vanbanduthao',         
-         children: [
-          {
-            path: '',
-            loadChildren:'src/app/members/vanbanduthao/vanbanduthao.module#VanbanduthaoPageModule',
-          },
-          {
-            path: 'vanbanduthaochuaxuly',
-            loadChildren: 'src/app/members/vanbanduthaochuaxuly/vanbanduthaochuaxuly.module#VanbanduthaochuaxulyPageModule',
-          },
-          {
-            path: 'vanbanduthaodangxuly',
-            loadChildren: 'src/app/members/vanbanduthaodangxuly/vanbanduthaodangxuly.module#VanbanduthaodangxulyPageModule',
-          },
-          {
-            path: 'vanbanduthaodaxuly',
-            loadChildren: 'src/app/members/vanbanduthaodaxuly/vanbanduthaodaxuly.module#VanbanduthaodaxulyPageModule',
-          },
-          {
-            path: 'vanbanduthaochitiet/:vanbanduthaoid',
-            loadChildren: 'src/app/members/vanbanduthaochitiet/vanbanduthaochitiet.module#VanbanduthaochitietPageModule',
-          },
-        ]
-         },
-      { path: 'danhba', loadChildren: 'src/app/members/danhba/danhba.module#DanhbaPageModule' },
-      { path: 'viewfile', loadChildren: 'src/app/members/viewfile/viewfile.module#ViewfilePageModule' },
-   //   { path: 'vanbandenchuaxuly', loadChildren: './vanbandenchuaxuly/vanbandenchuaxuly.module#VanbandenchuaxulyPageModule' },
-      
-    ]}
-];
+import { DirectivesModule } from 'src/app/directives/directives.module';
+import { AppTabsRouterModule } from './app-tabs-router.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    AppTabsRouterModule,
+    DirectivesModule
   ],
   declarations: [AppTabsPage]
 })
