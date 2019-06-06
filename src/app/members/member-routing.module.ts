1e884d93bd1434b8e025061e1895fe15debb84b7
dunghoang046 <hoangthuc046@gmail.com>
2019-06-06 08:59:30 +0700

    dunghv

--- src/app/members/member-routing.module.ts
+++ src/app/members/member-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
     path: '',
     loadChildren: './app-tabs/app-tabs.module#AppTabsRoutingModule',
     
-  }
+  },  { path: 'ykienvanbanden', loadChildren: './vanbanden/ykienvanbanden/ykienvanbanden.module#YkienvanbandenPageModule' }
+
 ];
 
 @NgModule({
---
+++ src/app/members/vanbanden/ykienvanbanden/ykienvanbanden.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ykienvanbanden',
+  templateUrl: './ykienvanbanden.page.html',
+  styleUrls: ['./ykienvanbanden.page.scss'],
+})
+export class YkienvanbandenPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
